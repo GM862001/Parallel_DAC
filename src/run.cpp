b6b4414600a275adf3aadfe0dbe7381e470ea4c5
gabrielemarino <gmarinogh.8601@gmail.com>
2022-12-20 10:25:12 +0100

    Added default values for simple run

--- src/run.cpp
+++ src/run.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+#include <unordered_map>
 #include "Mergesort.hpp"
+#include "utils.hpp"
 
 int main(int argc, char *argv[]){
 
-    if (argc < 5){
-        std::cout << "Usage: " << argv[0] << " <N> <B> <nw> <random_seed>" << std::endl;
+    if (argc < 3){
+        std::cout << "Usage: " << argv[0] << " <N> <nw>" << std::endl;
     }
 
     int N = atoi(argv[1]);
-    int B = atoi(argv[2]);
-    int nw = atoi(argv[3]);
-    int random_seed = atoi(argv[4]);
+    int nw = atoi(argv[2]);
+    int B;
+    int random_seed;
+
+    std::unordered_map<std::string, int*> params;
+    params["run_B"] = &B;
+    params["run_random_seed"] = &random_seed;
+    parse_config(params);
+
     int S = N / nw;
     long int time = run(N, B, S, random_seed);
     std::cout << "Time: " << time << " usecs" << std::endl;
